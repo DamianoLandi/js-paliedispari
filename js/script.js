11e25427c5ca198a726fa28c3c8b62335584db0e
DamianoLandi <damianolandi01@gmail.com>
2021-07-12 20:00:35 +0200

    Even Odd

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+//Palindrome
+
 var wordInput = prompt("Inserisci una parola");
 
 var result = document.getElementById("result");
@@ -23,3 +25,39 @@ if(palindromeCheck(wordInput)){
 }else{
     result.innerHTML = "non";
 }
+
+//EvenOdd
+
+var numbInput = parseInt(prompt("Inserisci un numero da 1 a 5"));
+
+var typeInput = prompt("Pari o Dispari?");
+
+
+if (typeInput.toLowerCase === "pari"){
+    var isEven = true;
+}else if (typeInput.toLowerCase === "dispari"){
+    var isEven = false;
+}else{
+    location.reload;
+}
+
+function evenOdd(numb, bool){
+    var cpuNumb = Math.floor(Math.random() *5) + 1;
+    var evenOddResult = "";
+    if (bool){
+        if((numb + cpuNumb) % 2 === 0){
+            evenOddResult = "Vince il Player";
+        }else{
+            evenOddResult = "Vince il CPU";
+        }
+    }else{
+        if((numb + cpuNumb) % 2 === 0){
+            evenOddResult = "vince il CPU";
+        }else{
+            evenOddResult = "Vince il Player"
+        }
+    }
+    return evenOddResult;
+}
+
+console.log(evenOdd(numbInput, isEven));
